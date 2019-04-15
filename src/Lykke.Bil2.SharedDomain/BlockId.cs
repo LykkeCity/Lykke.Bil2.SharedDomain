31e1f6b43faa1678678e67d08725b42437133c61
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-04-15 22:15:45 +0300

    BlockId and TransactionId

---
+++ src/Lykke.Bil2.SharedDomain/BlockId.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+
+namespace Lykke.Bil2.SharedDomain
+{
+    [PublicAPI]
+    
+    public sealed class BlockId : BaseImplicitToStringValueType<BlockId>
+    {
+        public BlockId(string value) :
+            base(value)
+        {
+        }
+
+        public static implicit operator BlockId(string value)
+        {
+            return value != null ? new BlockId(value) : null;
+        }
+   
+        public static implicit operator string(BlockId value)
+        {
+            return value?.Value;
+        }
+    }
+}
