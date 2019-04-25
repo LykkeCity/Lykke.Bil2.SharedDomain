46cb41d90e83a3941b54ce33a2dc03a7e796211e
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-04-25 17:06:17 +0300

    Transaction estimation error added

---
+++ src/Lykke.Bil2.SharedDomain/TransactionEstimationError.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace Lykke.Bil2.SharedDomain
+{
+    /// <summary>
+    /// Enum describing reason of the transaction estimation failure.
+    /// </summary>
+    [PublicAPI]
+    [JsonConverter(typeof(StringEnumConverter), typeof(CamelCaseNamingStrategy), new object[0], false)]
+    public enum TransactionEstimationError
+    {
+        /// <summary>
+        /// there is not enough balance on the some of the source addresses. Transaction can be estimated later,
+        /// once funds are refilled, with exactly the same parameters or amount to transfer should be reduced.
+        /// </summary>
+        [EnumMember(Value = "notEnoughBalance")]
+        NotEnoughBalance,
+    }
+}
