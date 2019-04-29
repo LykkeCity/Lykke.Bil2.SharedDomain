9d8e5af73fda2f4a70d204de9499b21ca55db228
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-04-29 23:56:40 +0300

    Base58String replaced with Base64String

---
+++ src/Lykke.Bil2.SharedDomain/Exceptions/Base64StringConversionException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lykke.Bil2.SharedDomain.Exceptions
+{
+    /// <summary>
+    /// Base64 string conversion exception
+    /// </summary>
+    public class Base64StringConversionException : Exception
+    {
+        /// <summary>
+        /// Base64 string conversion exception
+        /// </summary>
+        public Base64StringConversionException(string message, string sourceValue, Exception innerException = null) :
+            base(BuildMessage(message, sourceValue), innerException)
+        {
+        }
+
+        private static string BuildMessage(string message, string sourceValue)
+        {
+            return $"Value [{sourceValue}] can't be represented as base64 string: {message}";
+        }
+    }
+}
---
+++ tests/Lykke.Bil2.SharedDomain.Tests/Base64JsonConversionTests.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Lykke.Bil2.SharedDomain.Tests
+{
+    [TestFixture]
+    public class Base64JsonConversionTests
+    {
+        private class TypeWithBase64Property
+        {
+            public Base64String Base64Property { get; set; }
+        }
+
+        [Test]
+        [TestCase(null, ExpectedResult = "{\"Base64Property\":null}")]
+        [TestCase("", ExpectedResult = "{\"Base64Property\":\"\"}")]
+        [TestCase("the string", ExpectedResult = "{\"Base64Property\":\"dGhlIHN0cmluZw==\"}")]
+        public string Can_be_serialized(string stringValue)
+        {
+            var obj = new TypeWithBase64Property
+            {
+                Base64Property = Base64String.Encode(stringValue)
+            };
+
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        [Test]
+        [TestCase("{\"Base64Property\":null}", ExpectedResult = null)]
+        [TestCase("{\"Base64Property\":\"\"}", ExpectedResult = null)]
+        [TestCase("{\"Base64Property\":\"dGhlIHN0cmluZw==\"}", ExpectedResult = "the string")]
+        public string Can_be_deserialized(string json)
+        {
+            var obj = JsonConvert.DeserializeObject<TypeWithBase64Property>(json);
+
+            return obj.Base64Property?.DecodeToString();
+        }
+    }
+}
